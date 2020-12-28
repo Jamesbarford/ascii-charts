0c3298409c0ef6f20ee53e541af6bc7b2da099f2
Jamesbarford <jamesbarfordevans@gmail.com>
2020-12-28 19:04:35 +0000

    alternate implementation for csv reader

--- src/tokenizers/csv_tokenizer.hpp
+++ src/tokenizers/csv_tokenizer.hpp
@@ -6,5 +6,6 @@
 
 void traverse_csv(std::string path, std::function<void(std::string csv_string, size_t row)> iteratee);
 void split_row(std::string csv_string, std::function<void(std::string row_item, size_t column_idx)>);
+void csv_reader(std::string path, std::function<void(size_t row_idx, size_t column_idx, std::string value)> iteratee);
 
 #endif
